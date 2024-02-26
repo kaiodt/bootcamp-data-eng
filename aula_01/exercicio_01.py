8699d407998f1b330b4fd7dc432dcc3c9be28a55
Kaio Rocha <kaiodtr@gmail.com>
2024-02-26 11:59:45 -0300

    Code: Adição dos exercícios da Aula 01.

---
+++ aula_01/exercicio_01.py
@@ -0,0 +1,15 @@
+##############################################################################
+# Bootcamp de Python, SQL e Git para Engenharia de Dados                     #
+# Aula 01 - Variáveis em Python                                              #
+##############################################################################
+
+# Exercício 01 ###############################################################
+#                                                                            #
+# Crie um programa em que o usuário digita seu nome e o número de caracteres #
+# é exibido.                                                                 #
+#                                                                            #
+##############################################################################
+
+name = input("Digite o seu nome: ")
+print(f"Número de caracteres: {len(name)}")
+
---
+++ aula_01/exercicio_02.py
@@ -0,0 +1,17 @@
+##############################################################################
+# Bootcamp de Python, SQL e Git para Engenharia de Dados                     #
+# Aula 01 - Variáveis em Python                                              #
+##############################################################################
+
+# Exercício 02 ###############################################################
+#                                                                            #
+# Crie um programa onde o usuário digita dois valores e a soma deles é       #
+# retornada.                                                                 #
+#                                                                            #
+##############################################################################
+
+val_1 = int(input("Digite um valor: "))
+val_2 = int(input("Digite um valor: "))
+
+print(f"{val_1} + {val_2} = {val_1 + val_2}")
+
